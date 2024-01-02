6b1f92711a50eeba1658fc701862ee033f00dee5
Satish Nagraj <132422926+Nagrajx@users.noreply.github.com>
2024-01-03 00:08:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+async function getquate(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    // console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author
+}
+
+getquate(api_url);   
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text="+ quote.innerHTML + "----by" + author.innerHTML,"tweet Window" , "width=600px,height=300px")
+}
